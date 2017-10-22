a65892a5b4f8d212a3fe8f7a979398acbc6021d1
Niladri28dutta <nild2141@gmail.com>
2017-10-22 18:59:20 +0530

    add new repository and mock data for models

---
+++ ShoppingCart/src/ShoppingCart/Controllers/PieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+
+
+namespace ShoppingCart.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IPieReposiory _pieRepository;
+
+        public PieController(ICategoryRepository categoryRepository,IPieReposiory pieRepository)
+        {
+            this._categoryRepository = categoryRepository;
+            this._pieRepository = pieRepository;
+        }
+        public ViewResult Index()
+        {
+            return View(this._pieRepository.Pies);
+        }
+    }
+}
---
+++ ShoppingCart/src/ShoppingCart/Models/IPieReposiory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public interface IPieReposiory
+    {
+        IEnumerable<Pie> Pies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie GetPieById(int pieId);
+    }
+}
